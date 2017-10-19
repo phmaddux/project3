4f1a2029f30d6c360c1eacd1e7253fa440e5e5c6
Hunter Maddux <phmaddux@gmail.com>
2017-10-18 20:33:21 -0400

    project controller

---
+++ routes/ProjectsController.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const { ProjectModel } = require('../db/schema')
+const { ProjectVariantModel } = require('../db/schema')
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+    try {
+        const projects = await ProjectModel.find({})
+        res.json(projects)
+    } catch (err) {
+        res.send(err)
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        const project = await ProjectModel.findById(req.params.id)
+        res.json(project)
+    } catch (err) {
+        res.send(err)
+    }
+})
+
+module.exports = router;
