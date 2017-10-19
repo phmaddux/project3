4fc56d297155ca19f728bbcf1d58929c66df0f56
Hunter Maddux <phmaddux@gmail.com>
2017-10-18 20:16:53 -0400

    project page

--- client/src/components/ProjectPage/ProjectPage.js
+++ client/src/components/ProjectPage/ProjectPage.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+import styled from 'styled-components'
+
+const Heading = styled.div`
+font-size: 4.75rem;
+font-weight: Bold;
+margin: 30px 30px;
+`
+const ProjectsContainer = styled.div`
+`
+const ProjectStyle = styled.div`
+`
 
 class ProjectPage extends Component {
-  render () {
+  state = {
+    projects: []
+  }
+  componentWillMount() {
+    this.getAllProjects()
+  }
+  getAllProjects = async () => {
+    try {
+      const res = await axios.get('/api/projects')
+      this.setState({ projects: res.data })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+
+  render() {
     return (
       <div>
-        <h1>ProjectPage</h1>
+        <Heading>Project Types</Heading>
+        <ProjectsContainer>
+          {this.state.projects.map(project => {
+            return (
+              <ProjectStyle>
+                <img src={project.picture} alt="" />
+                <Link key={project._id} to={`/api/projects${project._id}`}>{project.name} </Link>
+              </ProjectStyle>
+            )
+          })}
+          </ProjectsContainer>
       </div>
     )
   }
